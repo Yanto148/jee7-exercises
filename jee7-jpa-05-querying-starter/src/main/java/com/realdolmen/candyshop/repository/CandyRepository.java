d66e4a7dc1ac3eca1b12fd5f6a88211ae101446d
Yanto Maes <yanto_maes@icloud.com>
2017-08-14 10:41:19 +0200

    Solved JPA Querying exercise

--- jee7-jpa-05-querying-starter/src/main/java/com/realdolmen/candyshop/repository/CandyRepository.java
+++ jee7-jpa-05-querying-starter/src/main/java/com/realdolmen/candyshop/repository/CandyRepository.java
@@ -4,6 +4,7 @@ import com.realdolmen.candyshop.domain.Candy;
 import com.realdolmen.candyshop.domain.Person;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
@@ -12,12 +13,15 @@ public class CandyRepository {
 
     public double findAverageCandyPrice() {
         // TODO: implement this method
-        return 0;
+        Query query = em.createQuery("SELECT avg(c.price) FROM Candy c", Double.class);
+        return (Double) query.getSingleResult();
     }
 
     public List<Candy> findCandyByNameLike(String name) {
         // TODO: implement this method
-        return null;
+        Query query = em.createQuery("SELECT c FROM Candy c WHERE c.name LIKE :name", Candy.class);
+        query.setParameter("name", '%' + name + '%');
+        return query.getResultList();
     }
 
     public List<Candy> findUniqueCandyForPersonOrderHistory(Person p) {
--- jee7-jpa-05-querying-starter/src/main/java/com/realdolmen/candyshop/repository/PersonRepository.java
+++ jee7-jpa-05-querying-starter/src/main/java/com/realdolmen/candyshop/repository/PersonRepository.java
@@ -3,6 +3,7 @@ package com.realdolmen.candyshop.repository;
 import com.realdolmen.candyshop.domain.Person;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.List;
 
 public class PersonRepository {
@@ -11,29 +12,40 @@ public class PersonRepository {
 
     public Person findPersonById(long id) {
         // TODO: implement this method
-        return null;
+        Query query = em.createQuery("SELECT p FROM Person p WHERE p.id = :id", Person.class);
+        query.setParameter("id", id);
+        return (Person) query.getSingleResult();
     }
 
     public List<Person> findAllPeople() {
         // TODO: implement this method
-        return null;
+        Query query = em.createQuery("SELECT p FROM Person p ORDER BY p.lastName, p.firstName", Person.class);
+        return query.getResultList();
     }
 
     public void savePerson(Person p) {
         // TODO: implement this method
+        em.persist(p);
     }
 
     public void deletePersonById(long id) {
         // TODO: implement this method
+        Query query = em.createQuery("SELECT p FROM Person p WHERE p.id = :id", Person.class);
+        query.setParameter("id", id);
+        Person person = (Person) query.getSingleResult();
+        if (person != null) em.remove(person);
     }
 
     public void updatePerson(Person p) {
         // TODO: implement this method
+        p = em.merge(p);
+
     }
 
     public long countAllPeople() {
         // TODO: implement this method
-        return 0;
+        List<Person> persons = this.findAllPeople();
+        return persons.size();
     }
 
 }
